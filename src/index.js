1f10624a1b7a429e69f19262cd5035f5f5549e01
xiaoguoguo <1662106538@qq.com>
2020-01-05 22:12:23 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import printMe from './print.js';
+import './style.css';
+import {cube} from './math';
+
+function component(){
+    var element = document.createElement('div');
+    element.innerHTML=cube(5);
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    return element;
+}
+let element = component();
+document.body.appendChild(element);
+if(module.hot){
+    module.hot.accept('./print.js',function(){
+        console.log('Accepting the updated printMe module!');
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    })
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin= require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+    entry: {
+      app:'./src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000,
+      hot:true
+    },
+    module:{
+      rules:[{
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      }]
+    },
+    plugins:[
+      new CleanWebpackPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new HtmlWebpackPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+}
